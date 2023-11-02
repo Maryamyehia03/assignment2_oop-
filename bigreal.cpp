5bfeb02d92054ec5e2190e3e07272fe2e463707c
Maryamyehia03 <114694952+Maryamyehia03@users.noreply.github.com>
2023-11-03 01:29:47 +0200

    Update bigreal.cpp

--- bigreal.cpp
+++ bigreal.cpp
@@ -4,26 +4,208 @@
 #include <iostream>
 using namespace std;
 
+//BigReal BigReal::operator+(BigReal other)
+//{
+//   
+//    string st1, st2;
+//    if (sign == other.sign) {
+//       st1 =to_string (stoi(s1) + stoi(other.s1));
+//       st2 =to_string (stoi(s2) + stoi(other.s2));
+//       return BigReal(sign, st1, st2);
+//    }
+//   /* else {
+//
+//    }*/
+//  
+//}
+
 void BigReal::setNum(string realNumber)
 {
     num = realNumber;
 }
 
-
-
-BigReal BigReal::operator+(BigReal other)
+bool BigReal::operator<(BigReal anotherReal)
 {
-   
-    string st1, st2;
-    if (sign == other.sign) {
-       st1 =to_string (stoi(s1) + stoi(other.s1));
-       st2 =to_string (stoi(s2) + stoi(other.s2));
-       return BigReal(sign, st1, st2);
+    if (sign == anotherReal.sign)
+    {
+        if (sign == '+' && anotherReal.sign == '+')
+        {
+            if (s1.size() > anotherReal.s1.size()) {
+                return false;
+            }
+            else if (s1.size() < anotherReal.s1.size()) {
+                return true;
+            }
+            else if (s1.size() == anotherReal.s1.size()) {
+                for (int j = 0; j < (s1.size(), anotherReal.s1.size()); j++)
+                {
+                    if (s1[j] > anotherReal.s1[j]) {
+                        return false;
+                    }
+                    else if (s1[j] < anotherReal.s1[j]) {
+                        return true;
+                    }
+                }
+            }
+            else {
+                for (int j = 1; j < max(s2.size(), anotherReal.s2.size()); j++)
+                {
+                    if (s2[j] > anotherReal.s2[j]) {
+                        return false;
+                    }
+                    else if (s2[j] < anotherReal.s2[j]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        else  if (sign == '-' && anotherReal.sign == '-') {
+            if (s1.size() < anotherReal.s1.size()) {
+                return false;
+            }
+            else if (s1.size() > anotherReal.s1.size()) {
+                return true;
+            }
+            else if (s1.size() == anotherReal.s1.size()) {
+                for (int j = 0; j < (s1.size(), anotherReal.s1.size()); j++)
+                {
+                    if (s1[j] < anotherReal.s1[j]) {
+                        return false;
+                    }
+                    else if (s1[j] > anotherReal.s1[j]) {
+                        return true;
+                    }
+                }
+            }
+            else {
+                for (int j = 1; j < max(s2.size(), anotherReal.s2.size()); j++)
+                {
+                    if (s2[j] < anotherReal.s2[j]) {
+                        return false;
+                    }
+                    else if (s2[j] > anotherReal.s2[j]) {
+                        return true;
+                    }
+                }
+            }
+        }
     }
-   /* else {
+    else if (sign == '+' && anotherReal.sign == '-')return false;
+    else return true;
 
-    }*/
-  
+}
+
+bool BigReal::operator>(BigReal anotherReal)
+{
+    if (sign == anotherReal.sign)
+    {
+        if (sign == '+' && anotherReal.sign == '+')
+        {
+            if (s1.size() > anotherReal.s1.size()) {
+                return true;
+            }
+            else if (s1.size() < anotherReal.s1.size()) {
+                return false;
+            }
+            else if (s1.size() == anotherReal.s1.size()) {
+                for (int j = 0; j < (s1.size(), anotherReal.s1.size()); j++)
+                {
+                    if (s1[j] > anotherReal.s1[j]) {
+                        return true;
+                    }
+                    else if (s1[j] < anotherReal.s1[j]) {
+                        return false;
+                    }
+                }
+            }
+            else {
+                for (int j = 1; j < max(s2.size(), anotherReal.s2.size()); j++)
+                {
+                    if (s2[j] > anotherReal.s2[j]) {
+                        return true;
+                    }
+                    else if (s2[j] < anotherReal.s2[j]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        else if (sign = '-' && anotherReal.sign == '-') {
+            if (s1.size() < anotherReal.s1.size()) {
+                return true;
+            }
+            else if (s1.size() > anotherReal.s1.size()) {
+                return false;
+            }
+            else if (s1.size() == anotherReal.s1.size()) {
+                for (int j = 0; j < (s1.size(), anotherReal.s1.size()); j++)
+                {
+                    if (s1[j] < anotherReal.s1[j]) {
+                        return true;
+                    }
+                    else if (s1[j] > anotherReal.s1[j]) {
+                        return false;
+                    }
+                }
+            }
+            else {
+                for (int j = 1; j < max(s2.size(), anotherReal.s2.size()); j++)
+                {
+                    if (s2[j] < anotherReal.s2[j]) {
+                        return true;
+                    }
+                    else if (s2[j] > anotherReal.s2[j]) {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    else if (sign == '+' && anotherReal.sign == '-') return true;
+    else if (sign = '-' && anotherReal.sign == '+')return false;
+}
+
+bool BigReal::operator==(BigReal anotherReal)
+{
+    if (sign != anotherReal.sign)
+    {
+        return false;
+    }
+    if (s1.size()!=anotherReal.s1.size())
+    {
+        return false;
+    }
+    else {
+        for (int j = 0; j <s1.size(); j++)
+        {
+            if (s1[j] > anotherReal.s1[j]) {
+                return false;
+            }
+            else if (s1[j] < anotherReal.s1[j]) {
+                return false;
+            }
+        }
+    }
+     if (s2.size()!=anotherReal.s2.size())
+    {
+        return false;
+    }
+     else {
+         for (int j = 1; j < s2.size(); j++)
+         {
+             if (s2[j] > anotherReal.s2[j]) {
+                 return false;
+             }
+             else if (s2[j] < anotherReal.s2[j]) {
+                 return false;
+             }
+         }
+     }
+     if (sign == anotherReal.sign)
+     {
+         return true;
+     }
+    
 }
 
 bool BigReal::isvalid()
