68da92fa82e096dc00e6e04b61379d026fa23cf2
ayaaasayeed <147319493+ayaaasayeed@users.noreply.github.com>
2023-11-05 15:14:59 +0200

    Update bigreal.h

--- bigreal.h
+++ bigreal.h
@@ -8,10 +8,12 @@ private:
     char sign;
     string s1, s2, num;
 public:
+        BigReal(double realNumber=0.0);
+        BigReal(string realNumber);
 	//friend ostream& operator << (ostream& out, BigReal num);
 	//BigReal operator+ (BigReal other);
 	//BigReal operator- (BigReal& other);
-	//BigReal(const BigReal& other); // Copy constructor
+	BigReal(const BigReal& other); // Copy constructor
 	//BigReal& operator= (BigReal& other); // Assignment operator
 	//// Depending on how you store data, default can be enough
 	void setNum(string realNumber);
