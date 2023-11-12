94ddfee372ea6c1237e8d84034880fe5fa9ab0ba
Maryamyehia03 <114694952+Maryamyehia03@users.noreply.github.com>
2023-11-12 22:00:12 +0200

    Update instruction.cpp

--- instruction.cpp
+++ instruction.cpp
@@ -1,24 +1,67 @@
 #include "instruction.h"
-#include "registers.h"
-#include "memory.h"
+//#include "registers.h"
+//#include "memory.h"
+#include "bitset"
 #include<iostream>
+
 using namespace std;
+
+int instruction::addTwoBinary(int num1, int num2)
+{
+    // Convert integers to binary strings
+    std::bitset<32> binary1(num1);
+    std::bitset<32> binary2(num2);
+
+    // Perform binary addition
+    std::bitset<32> sum = binary1.to_ulong() + binary2.to_ulong();
+
+    // Check for overflow
+    if (sum[31] != sum[30]) {
+        std::cerr << "Overflow occurred!" << std::endl;
+        // Handle overflow as needed
+    }
+
+    // Return the result as an integer
+    return static_cast<int>(sum.to_ulong());
+}
+
 
 void instruction::instr(string s)
 {
-    
-        // while (s != "c000")
-       //  {
-        if (s[0] == '1')
-        {
-            registers.load1r(s[1], memory.print(conv2(s[2], s[3])));
-            cout << registers.read(s[1]) << "\n";
-        }
-        else if (s[0] == '2')
-        {
-            registers.load1r(s[1], con1(conv2(s[2], s[3])));
-            cout << registers.read(s[1]) << "\n";
-        }
-    
-       // }
+
+
+    // while (s != "c000")
+    //  {
+    if (s[0] == '1')
+    {
+        registers00.load1r(s[1], memory.print(conv2(s[2], s[3])));
+        cout << registers00.read(s[1]) << "\n";
+    }
+    else if (s[0] == '2')
+    { cout<<s[1]<<" "<<s[2]<<" "<<s[3]<<"\n";
+        registers00.load1r(s[1], con1(conv2(s[2], s[3])));
+        cout << registers00.read(s[1]) << "\n";
+    }
+    else if (s[0]=='3')
+    {
+
+    }
+    else if (s[0] == '4')
+    {
+
+    }
+    else if (s[0]=='5')
+    {
+        registers00.load1r(s[1], addTwoBinary(registers00.read(s[2]), registers00.read(s[3])));
+        cout << registers00.read(s[1]) << "\n";
+    }
+    else if (s[0]=='B')
+    {
+
+    }
+
+     //}
 }
+
+
+
