3859bf20578b0728840f2c9ab163a8ee55fad608
Maryamyehia03 <114694952+Maryamyehia03@users.noreply.github.com>
2023-11-12 21:59:54 +0200

    Update instruction.h

--- instruction.h
+++ instruction.h
@@ -4,28 +4,37 @@
 #include<string>
 #include<cmath>
 #include<algorithm>
+#include<stdio.h>
+#include<stddef.h>
+#include<cstdio>
+#include "bitset"
+#include "registers.h"
+#include "memory.h"
 using namespace std;
-class instruction 
+class instruction
 {
-private :
-	registers registers;
-	memory memory;
-protected:
-	//convert hexa as a string to int
-	int con1(string hexa) {
-		int ans;
-		sscanf(hexa.c_str(), "%x", &ans);
-		return ans;
-	}
-	//collect two char to one string 
-	string conv2(char c1, char c2) {
-		string coll;
-		coll.push_back(c1 + c2);
-		return coll;
-	}
-public:
+private:
+    //convert hexa as a string to int
+    int con1(string hexa) {
+        int ans;
+        sscanf(hexa.c_str(), "%x", &ans);
 
-	void instr(string s);
+        return ans;
+    }
+    //collect two char to one string
+    string conv2(char c1, char c2) {
+        string coll;
+        coll.push_back(c1);
+        coll.push_back(c2);
+
+        return coll;
+    }
+
+public:
+    registers registers00;
+    memory memory;
+    void instr(string s);
+    int addTwoBinary(int num1, int num2);
 
 };
 
