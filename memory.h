77271df058f69a3937feb025e8ef22f78043e86a
Maryamyehia03 <114694952+Maryamyehia03@users.noreply.github.com>
2023-11-13 16:15:22 +0200

    Update memory.h

--- memory.h
+++ memory.h
@@ -2,16 +2,43 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+//#include "registers.h"
+//#include "instruction.h"
+#include <string>
+#include <sstream>
 using namespace std;
 class memory
 {
 private:
-
+    //conver int to hexa as a string
+    string hex(int n){
+        char hex_string[20];
+        sprintf(hex_string, "%X", n); //convert number to hex
+        return hex_string;
+    }
+    //convert hexa as a string to int
+    int con1(string hexa) {
+        int ans;
+        sscanf(hexa.c_str(), "%x", &ans);
+        return ans;
+    }
+    //collect two char to one string
+    string conv2(char c1, char c2) {
+        string coll;
+        coll.push_back(c1);
+        coll.push_back(c2);
+        return coll;
+    }
     int cnt = 0;
-    vector <pair<string, string>>vm={{"00","00"},{"01","00"}};
-
+    vector<pair<string, string>> v;
 public:
-    int print(string address);
+   memory();
+    void storev(string value);
+    string printhexa(string address);
+     int printint(string address);
+    void read();
+void loadm(string add,string val);
     friend class instruction;
+
 };
 
