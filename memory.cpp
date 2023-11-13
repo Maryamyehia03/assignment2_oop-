30af96e8f289abb53136c8e187e361b8759fecce
Maryamyehia03 <114694952+Maryamyehia03@users.noreply.github.com>
2023-11-13 16:15:43 +0200

    Update memory.cpp

--- memory.cpp
+++ memory.cpp
@@ -3,25 +3,73 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
 
-int memory::print(string address)
-{
-    if (cnt > 255)
-    {
-        cout << "Sorry! The memory is full\n"; return 0;
+memory::memory() {
+    for (int i = 0; i < 256; i++) {
+        if (i<16){
+            v.push_back({conv2('0',hex(i)[0]),"00"});
+            continue;
+        }
+        v.push_back({hex(i),"00"});
     }
-    cnt++;
-    for (auto it : vm) {
-        if (it.first == address)
-        {
-            string s = it.second;
-            int ans;
-            sscanf(s.c_str(), "%x", &ans);
-            return(ans);
+}
 
+void memory::storev(string value) {
+    v[cnt].second=conv2(value[0],value[1]);
+    cnt++;
+
+    v[cnt].second= conv2(value[2],value[3]);
+    cnt++;
+
+}
+
+int memory::printint(string address) {
+
+    for (auto it:v) {
+        if (it.first==address){
+            int ans;
+             sscanf(it.second.c_str(), "%x", &ans);
+             sscanf(it.second.c_str(), "%X", &ans);
+            return ans;
         }
     }
-    cout << "The address isn't found\n";
+    cout<<"The address isn't found\n";
     return 0;
 }
+
+string memory::printhexa(string address) {
+    for (auto it:v) {
+        if (it.first==address){
+            return it.second;
+        }
+    }
+    cout<< "The address isn't found\n";
+}
+
+void memory::read() {
+    for (int i = 0; i < 256; i++) {
+       cout<< v[i].first<<" "<<v[i].second<<"\n";
+    }
+}
+
+void memory::loadm(string add, string val) {
+    for (auto &it:v) {
+        if (it.first==add){
+            it.second=val;
+            return;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
