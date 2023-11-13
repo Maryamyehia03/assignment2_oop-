bbd0cd7f14b6c0f191189a258f69fc6148d12e47
Maryamyehia03 <114694952+Maryamyehia03@users.noreply.github.com>
2023-11-13 16:14:28 +0200

    Update instruction.h

--- instruction.h
+++ instruction.h
@@ -14,20 +14,11 @@ using namespace std;
 class instruction
 {
 private:
-    //convert hexa as a string to int
-    int con1(string hexa) {
-        int ans;
-        sscanf(hexa.c_str(), "%x", &ans);
-
-        return ans;
-    }
-    //collect two char to one string
-    string conv2(char c1, char c2) {
-        string coll;
-        coll.push_back(c1);
-        coll.push_back(c2);
-
-        return coll;
+//conver int to hexa as a string
+    string hex(int n){
+        char hex_string[20];
+        sprintf(hex_string, "%X", n); //convert number to hex
+        return hex_string;
     }
 
 public:
@@ -35,6 +26,18 @@ public:
     memory memory;
     void instr(string s);
     int addTwoBinary(int num1, int num2);
-
+//convert hexa as a string to int
+    int con1(string hexa) {
+        int ans;
+        sscanf(hexa.c_str(), "%x", &ans);
+        return ans;
+    }
+    //collect two char to one string
+    string conv2(char c1, char c2) {
+        string coll;
+        coll.push_back(c1);
+        coll.push_back(c2);
+        return coll;
+    }
 };
 
