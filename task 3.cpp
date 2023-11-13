75be157315f1ba3bf574e79b5804095bff5ea28d
Maryamyehia03 <114694952+Maryamyehia03@users.noreply.github.com>
2023-11-13 16:12:52 +0200

    Update task 3.cpp

--- task 3.cpp
+++ task 3.cpp
@@ -1,17 +1,36 @@
-
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
 #include "registers.h"
 #include "memory.h"
 #include "instruction.h"
 #include <vector>
 #include <iostream>
+#include<fstream>
 using namespace std;
+int main() {
 
-int main()
-{
+string s,name;
+instruction instruction;
+cout<<"Please enter your file name: \n";
+cin>>name;
+    fstream file(name+".txt");
+    if (!file.is_open()){cout<<"Please enter the right name file\n";return 0;}
 
-	instruction instruction;
-	string s;
-	cin >> s;
-	instruction.instr(s);
+    while (file) {
+        getline(file,s);
 
+        if (s=="C000")break;
+        instruction.instr(s);
+    }
+    instruction.memory.storev("C000");
+    cout<<"memory is :\n";
+instruction.memory.read();
+
+ cout<<"Register is :\n";
+instruction.registers00.print();
+
+    file.close();
+
+    return 0;
 }
